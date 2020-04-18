47449a3ec5cc82b03c4aebcdc8dc4ae89524b5df
andre3992 <andre.regedor@hotmail.com>
2020-04-18 23:00:53 +0100

    changes

--- src/backend/index.js
+++ src/backend/index.js
@@ -55,5 +55,6 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 app.use('/', router)
-    // app.listen(3002)
+app.listen(3002)
 module.exports.handler = serverless(router);
+module.exports.handler = serverless(app);
