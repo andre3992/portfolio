21b92188fe9f02a227ad1b5e3f2aeae52673afd6
andre3992 <andre.regedor@hotmail.com>
2020-04-19 17:24:47 +0100

    changes

--- src/form/ContactForm.js
+++ src/form/ContactForm.js
@@ -1,10 +1,6 @@
 import React from 'react';
 import axios from 'axios';
-const express = require('express');
-const router = express.Router();
-const nodemailer = require('nodemailer');
-const cors = require('cors');
-const creds = require('../backend/config');
+
 class ContactForm extends React.Component{
   
   constructor(props) {
@@ -20,7 +16,7 @@ class ContactForm extends React.Component{
     e.preventDefault();
     axios({
       method: "POST", 
-      url:"https://loving-hamilton-296054.netlify.app/send", 
+      url:"https://portfolioandre.herokuapp.com/send", 
       data:  this.state
     }).then((response)=>{
       if (response.data.status === 'success'){
@@ -71,56 +67,5 @@ class ContactForm extends React.Component{
 	this.setState({message: event.target.value})
   }
 }
-const transport = {
-    host: "smtp.office365.com", // Don’t forget to replace with the SMTP host of your provider
-    port: 587,
-    auth: {
-      user: creds.USER,
-      pass: creds.PASS,
-    }
-  }
-  
-  const transporter = nodemailer.createTransport(transport);
-  
-  transporter.verify((error, success) => {
-    if (error) {
-      console.log(error);
-    } else {
-      console.log("Server is ready to take messages");
-    }
-  });
-  
-    exports.handler = router.post("/send",(req, res, next) => {
-      const name = req.body.name;
-      const email = req.body.email;
-      const message = req.body.message;
-      const content = `name: ${name} \n email: ${email} \n message: ${message} `;
-  
-      const mail = {
-        from: name,
-        to: "andre.regedor@hotmail.com", // Change to email address that you want to receive messages on
-        subject: "New Message from Contact Form",
-        text: content,
-      };
-  
-      transporter.sendMail(mail, (err, data) => {
-        if (err) {
-          res.json({
-            status: "fail",
-          });
-        } else {
-          res.json({
-            status: "success",
-          });
-        }
-      });
-    });
-  
-  const app = express();
-  app.use(cors());
-  app.use(express.json());
-  app.use("/", router);
-  app.listen(3002);
-
 
 export default ContactForm;
